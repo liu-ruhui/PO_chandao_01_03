25efc27d1d21c2b755671ff09608b9476a997c4b
LRH <1263902331@qq.com>
2020-04-29 22:48:33 +0800

    第五次提交： 1.更改login_page元素从ymal文件获取

---
+++ common/element_yaml_utils.py
@@ -0,0 +1,21 @@
+import os
+import yaml
+
+current_path = os.path.dirname(__file__)
+yaml_path = os.path.join(current_path, '..//elemnt_info_datas/element_infos.yaml')
+
+
+
+def read_yaml(page_name,element_path=yaml_path):
+    file = open(element_path, 'r', encoding='utf-8')
+    yaml_content = file.read()
+    dict_yaml= yaml.safe_load(yaml_content)
+    elements = dict_yaml[page_name]
+    return elements
+
+if __name__ == '__main__':
+    e = read_yaml('login_page')
+    print(e)
+
+
+
